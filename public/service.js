4b9ae536a75efe09e53e3999c43381a17262ec69
jsvalle123 <68571871+jsvalle123@users.noreply.github.com>
2021-02-25 22:58:14 -0800

    heroku

--- public/service.js
+++ public/service.js
@@ -1,11 +1,11 @@
 const FILES_CACHE = [
     "/",
-    "/db.js",
-    "/index.html",
-    "/style.css",
-    "/index.js",
-    "/icons/icon-192x192.png",
-    "/icons/icon-512x512.png"
+    "./db.js",
+    "./index.html",
+    "./style.css",
+    "./index.js",
+    "./icons/icon-192x192.png",
+    "./icons/icon-512x512.png"
 ];
 
 const CACHE_NAME = "static-cache-v2";
